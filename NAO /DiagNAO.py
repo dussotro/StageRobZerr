2ba6423384df7d72b960e7070cfd134923c18191
Victor BARES <victor.bares@ensta-bretagne.org>
2017-06-19 13:36:58 +0200

    Last maj 13h36

--- NAO /DiagNAO.py
+++ NAO /DiagNAO.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 import time
-import sys
+import sys 
 from naoqi import ALProxy
 import motion
 import select
@@ -7,6 +8,9 @@ import select
 robotIP = "172.20.12.126"
 port = 9559
 Frequency = 0.0 #low speed
+
+t = 1
+
 
 try:
     motionProxy = ALProxy("ALMotion", robotIP, port)
@@ -167,20 +171,37 @@ def doStop():
 
     
 if __name__== "__main__":
+    
     doInitialisation()
+    
     #test de la vision du NAO
+    
     #Test_Detection()
+    Test_detection()
+    time.sleep(t)
     
     #test de capteurs 
     TrySensors()
+    time.sleep(t)
     
     #test de déplacements
     dorun()
+    time.sleep(t)
+    
     doback()
+    time.sleep(t)
+    
     doleft()
+    time.sleep(t)
+    
     doright()
+    time.sleep(t)
+    
     doStandUp()
+    time.sleep()
+    
     doStop()
+    time.sleep()
     
 
 
