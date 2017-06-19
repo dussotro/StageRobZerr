a7421b24354081bd53e5d25e8686615afd4757f9
Romain DUSSOT <dussotro@tdell137.ensieta.ecole>
2017-06-19 10:23:34 +0200

    Ajout du Test de la detection

--- NAO /DiagNAO.py
+++ NAO /DiagNAO.py
@@ -31,19 +31,90 @@ def StiffnessOn(proxy):
     proxy.stiffnessInterpolation(pNames, pStiffnessLists, pTimeLists)
 
 
-# functions (actions of the fsm)
 def doInitialisation():
     print ">>>>>> Initialisation"   
-    
     # Set NAO in Stiffness On
     StiffnessOn(motionProxy)
     # Send NAO to Pose Init
     postureProxy.goToPosture("StandInit", 0.5)
-    return event # return event to be able to define the transition
 
+	
+"""""""""
+Vision
+"""""""""
+
+def Test_Detection():
+
+    # Create a proxy to ALFaceDetection
+    try:
+        faceProxy = ALProxy("ALFaceDetection", IP, PORT)
+    except Exception, e:
+        print "Error when creating face detection proxy:"
+        print str(e)
+        exit(1) 
+
+
+    period = 500
+    faceProxy.subscribe("Test_Face", period, 0.0 )
+
+    # Create a proxy to ALMemory
+    try:
+        memoryProxy = ALProxy("ALMemory", IP, PORT)
+    except Exception, e:
+        print "Error when creating memory proxy:"
+        print str(e)
+        exit(1)
+
+    # A simple loop that reads the memValue and checks whether faces are detected.
+    for i in range(0, 20):
+        time.sleep(0.5)
+        val = memoryProxy.getData(memValue)
+
+        print ""
+        print "*****"
+        print ""
+
+        # Check whether we got a valid output.
+        if(val and isinstance(val, list) and len(val) >= 2):
+
+        # We detected faces !
+        # For each face, we can read its shape info and ID.
+
+        # First Field = TimeStamp.
+        timeStamp = val[0]
+
+        # Second Field = array of face_Info's.
+        faceInfoArray = val[1]
+
+            try:
+                # Browse the faceInfoArray to get info on each detected face.
+                for j in range( len(faceInfoArray)-1 ):
+                    faceInfo = faceInfoArray[j]
+                    # First Field = Shape info.
+                    faceShapeInfo = faceInfo[0]
+
+                    # Second Field = Extra info (empty for now).
+                    faceExtraInfo = faceInfo[1]
+
+                print "  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
+                print "  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
+ 
+            except Exception, e:
+                print "faces detected, but it seems getData is invalid. ALValue ="
+                print val
+                print "Error msg %s" % (str(e))
+          else:
+              print "No face detected"
+
+        # Unsubscribe the module.
+        faceProxy.unsubscribe("Test_Face")
+
+        print "Detection finished"
 
 if __name__== "__main__":
-    
+    doInitialisation()
+    #test de la vision du NAO
+    Test_Detection()
 
 
 
