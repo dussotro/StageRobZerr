d7cdd3852d7c0a650e61ccc8342951c9d46c8ce7
dussotro <dussotro@tdell137.ensieta.ecole>
2017-07-05 13:45:09 +0200

    maj romain

--- Test Cam/nao_grab_cam.py
+++ Test Cam/nao_grab_cam.py
@@ -11,15 +11,8 @@ import pylab as pl
 import almath
 
 # NAO parameters
-<<<<<<< HEAD
+#robotIP, port = "172.20.27.47", 9559
 robotIP, port = "172.20.25.150", 9559
-=======
-<<<<<<< HEAD
-robotIP, port = "172.20.11.237", 9559
-=======
-robotIP, port = "172.20.13.63", 9559
->>>>>>> 831cfa681d613346052bc7213e2b5b876c57e2e1
->>>>>>> 0ee8a54027254e46d10507de394bc6f36b1aa046
 ALMEMORY_KEY_NAMES = ["Device/SubDeviceList/HeadYaw/Position/Sensor/Value", "Device/SubDeviceList/HeadPitch/Position/Sensor/Value"]
 
 # Detection threshold and counter parameters
@@ -86,7 +79,7 @@ except Exception, e:
     print "Error was: ", e
 try :
     tts = ALProxy("ALTextToSpeech", robotIP, port)
-    tts.setLanguage("English")
+    tts.setLanguage("French")
 except Exception, e: 
     print "Could not create proxy to ALTextToSpeech"
     print "Error was: ", e
@@ -117,11 +110,11 @@ VideoProxy.setParam(18, 0) # "kCameraSelectID", 0 : camera top, 1 : camera botto
 resolution = 0  # 0 : QQVGA, 1 : QVGA, 2 : VGA
 colorSpace = 11 # RGB
 
-try :
-    videoClient = VideoProxy.subscribe("python_client", resolution, colorSpace, 5)
-except RuntimeError:
-    camRepair.rep(robotIP, port)
-    videoClient = VideoProxy.subscribe("python_client", resolution, colorSpace, 5)
+#try :
+videoClient = VideoProxy.subscribe("python_client", resolution, colorSpace, 5)
+#except RuntimeError:
+#    camRepair.rep(robotIP, port)
+#    videoClient = VideoProxy.subscribe("python_client", resolution, colorSpace, 5)
 
 from_video = VideoProxy.getImageRemote(videoClient)
 imageWidth, imageHeight = from_video[0], from_video[1]
@@ -139,16 +132,10 @@ def Main():
     motionProxy.setWalkArmsEnabled(True, True)
     motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
     img_PIL1, img_PIL2, img_PIL3, img_PIL4 = np.zeros((imageWidth, imageHeight)), np.zeros((imageWidth, imageHeight)), np.zeros((imageWidth, imageHeight)), np.zeros((imageWidth, imageHeight))
-    pl.figure()
-    pl.subplot(221)
-    pl.imshow(img_PIL1)
-    pl.subplot(222)
-    pl.imshow(img_PIL2)
-    pl.subplot(223)
-    pl.imshow(img_PIL3)
-    pl.subplot(224)
-    pl.imshow(img_PIL4)
-    
+    f = pl.figure()
+    ax = f.gca()
+    f.show()
+        
     while isRuning: 
         filein=open("cmd","r")
         cmd=filein.readline().split()[0]
@@ -167,31 +154,33 @@ def Main():
             from_video = VideoProxy.getImageRemote(videoClient)
             img_nao = from_video[6]
             
-            pl.figure()
-            
+            pl.subplot(221)
             img_PIL1 = Image.frombytes("RGB", (imageWidth, imageHeight), img_nao)
+            pl.imshow(img_PIL1)
 #            pl.figure(figsize=(4,4))
 #            pl.imshow(img_PIL)
 #            pl.colorbar()
 #            pl.savefig('Image/img_PIL1.png')
-            pl.draw()
             
             
-<<<<<<< HEAD
+            
+            pl.subplot(222)
             img_PIL2 = cv2.cvtColor(np.asarray(img_PIL1), cv2.COLOR_RGB2HSV)
+            pl.imshow(img_PIL2)
 #            pl.figure(figsize=(4,4))
 #            pl.imshow(img_PIL)
 #            pl.colorbar()
 #            pl.savefig('Image/img_PIL2.png')
-            pl.draw()            
+                       
 
-            
+            pl.subplot(223)
             img_PIL3 = cv2.blur(img_PIL2, (5,5))
+            pl.imshow(img_PIL3)
 #            pl.figure(figsize=(4,4))
 #            pl.imshow(img_PIL)
 #            pl.colorbar()
 #            pl.savefig('Image/img_PIL3.png')
-            pl.draw()
+            
 
 #            hue = cv2.split(img_PIL)[0]
 #            sat = cv2.split(img_PIL)[1] 
@@ -210,32 +199,16 @@ def Main():
 #            pl.colorbar()   
 #            
 #            pl.savefig('Image/HSV.png')
-=======
-            pl.figure(figsize=(12,4))
-            pl.subplot(131)
-            pl.imshow(hue, cmap='Greys')
-            pl.subplot(132)
-            pl.imshow(sat, cmap='Greys')            
-            pl.subplot(133)
-            pl.imshow(val, cmap='Greys')
-                        
-            pl.savefig('Image/HSV.png')
-            
-            seuil = 100.0
-            ret, seg_sat = cv2.threshold(sat, seuil, 255.0, cv2.THRESH_BINARY)
-            pl.figure(figsize=(4,4))
-            pl.imshow(seg_sat)
-            pl.savefig('Image/IsolementBalle.png')
-            time.sleep(3)
->>>>>>> 0ee8a54027254e46d10507de394bc6f36b1aa046
 
-            
+
+            pl.subplot(224)
             img_PIL4 = cv2.inRange(img_PIL3, HSVmin, HSVmax)
+            pl.imshow(img_PIL4)
 #            pl.figure(figsize=(4,4)) 
 #            pl.imshow(img_PIL4)
 #            pl.colorbar()
 #            pl.savefig('Image/img_PIL4.png')
-            pl.draw()
+            f.canvas.draw()
             
             
             contours, hierarchy = cv2.findContours(img_PIL4, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -269,11 +242,8 @@ def Main():
                 x, y = recordData()
                 x, y = x + dx, y + dy
                 omega = x
-<<<<<<< HEAD
                 print " | dx : ", dx, " | dy : ", dy
-=======
-                print "aire : ", best_area, " | dx : ", dx, " | dy : ", dy
->>>>>>> 0ee8a54027254e46d10507de394bc6f36b1aa046
+
                 motionProxy.setAngles("HeadYaw", -x*almath.TO_RAD, 0.2)
                 motionProxy.setAngles("HeadPitch", y*almath.TO_RAD, 0.2)
             
@@ -305,13 +275,10 @@ def Main():
 #            filein.write("stop")
 #            filein.close() 
             
-<<<<<<< HEAD
             time.sleep(0.2)
-=======
-            time.sleep(5)
->>>>>>> 0ee8a54027254e46d10507de394bc6f36b1aa046
             
         elif cmd=="stop":
+            print "je m'arrete"
             motionProxy.stopMove()
             camRepair.rep(robotIP, port)
             rest.rest(robotIP, port)
