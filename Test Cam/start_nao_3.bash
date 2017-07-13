d35439e33d78efb53fc0b236ae22181b656bc201
dussotro <dussotro@tdell137.ensieta.ecole>
2017-07-13 16:11:43 +0200

    maj romain fin de stage

---
+++ Test Cam/start_nao_3.bash
@@ -0,0 +1,5 @@
+echo "Initialisation 3 - suit bleu"
+. ./setup_python_v2.bash
+export LD_LIBRARY_PATH="/home21/dussotro/Bureau/Stage/StageRobZerr/Test Cam/pynaoqi-python2.7-2.1.4.13-linux64"
+python nao_grab_cam_bleu.py 172.20.25.152
+
